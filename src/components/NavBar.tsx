38e321df46ee2cab201b0366476f312589d28215
YoderBy <yosefbyd@gmail.com>
2023-09-17 20:03:16 +0300

    building videocards

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from '../assets/LOGO.png'
-import {HStack, Image} from '@chakra-ui/react'
+import {HStack, Heading, Image} from '@chakra-ui/react'
 import ColorModeSwitch from "./ColorModeSwitch";
 const NavBar = () => {
     return (
         <HStack>
             <ColorModeSwitch></ColorModeSwitch>
-            <Image src = {logo} boxSize ='60px'/>
+            <Heading alignSelf={'center'}>יוסף בן יהודה</Heading>
         </HStack>
     )
 }
