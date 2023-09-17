5a64e92255774600c487204eb4ab2ded313010bb
YoderBy <yosefbyd@gmail.com>
2023-09-17 22:36:47 +0300

    Somehoy working

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,31 @@
+// GameGrid.tsx
 
 import { useEffect, useState } from 'react';
-import useVideos from '../Hooks/useVideos';
+import useVideos, { Video } from '../Hooks/useVideos';
 import { SimpleGrid } from '@chakra-ui/react';
 import GameCard from './GameCard';
-    const GameGrid = () => {
-        const {videos} = useVideos();
-    return (
+
+interface Props {
+  refreshing: boolean;
+}
+
+const GameGrid: React.FC<Props> = ({ refreshing }) => {
+        const videos = useVideos({ refreshing});
+        
+return (
     <>
-    <SimpleGrid dir='rtl' padding = {'10px'} columns={{sm:2, md:4, lg:5, xl:12}} spacing = {10}>
-        {videos.map(vid => <GameCard  key = {vid.id} video={vid}/>)}
-    </SimpleGrid>
+      <SimpleGrid
+        dir="rtl"
+        padding={'10px'}
+        columns={{ sm: 2, md: 4, lg: 5, xl: 12 }}
+        spacing={10}
+      >
+        {videos.map((vid) => (
+          <GameCard key={vid.id} video={vid} />
+        ))}
+      </SimpleGrid>
     </>
-)}
+  );
+};
 
 export default GameGrid;
