04781c8e51efc847a3d0d4c058567a0d56834724
YoderBy <yosefbyd@gmail.com>
2023-09-17 19:35:49 +0300

    added json functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './assets/Panopto_data.json';
 import {Show, Grid, GridItem, Button, ButtonGroup } from '@chakra-ui/react'
 import NavBar from './components/NavBar';
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
-
+import GameGrid from './components/GameGrid';
 
 
 function App() {
+const dat = {data};
+  
 return (<>
     <NavBar></NavBar>
     <Grid templateAreas={{
@@ -15,10 +18,12 @@ return (<>
       lg: `"nav nav" "aside main"`
       }
     }>
-    
+      
       <GridItem area='nav'>Nav</GridItem>
       <Show above='lg'><GridItem area='aside' bg = 'gold'>Aside</GridItem> </Show>
-      <GridItem area='main' bg = 'dodgerblue'>Nav</GridItem>
+      <GridItem area='main' bg = 'dodgerblue'>
+      <GameGrid></GameGrid>
+      </GridItem>
     </Grid>
     </>
     )
