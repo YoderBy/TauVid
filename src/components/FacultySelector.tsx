9206b80432367fa31aa6baad5647ee1f6b85d87a
YoderBy <yosefbyd@gmail.com>
2023-09-27 14:23:03 +0300

    fixing minor issues with dropdown

--- src/components/FacultySelector.tsx
+++ src/components/FacultySelector.tsx
@@ -2,14 +2,16 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import useFaculties, { Faculty } from "../Hooks/useFaculties";
 import { FaChevronDown as ChevronDownIcon } from 'react-icons/fa';
 interface Props {
+    selected_Faculty : Faculty|null;
     onSelect: (Faculty: Faculty) => void;
 }
 
-const FacultySelector = ({ onSelect }: Props) => {
+const FacultySelector = ({ onSelect, selected_Faculty }: Props) => {
     const Faculties = useFaculties();
     return (
         <Menu>
-            <MenuButton  rightIcon={<ChevronDownIcon />} as={Button}>פקולוטות</MenuButton>
+            <MenuButton  rightIcon={<ChevronDownIcon />} as={Button}>{
+            selected_Faculty?.name || 'פקולטות'}</MenuButton>
             <MenuList>
                 {Faculties.map(faculty => 
                 <MenuItem onClick={() => onSelect(faculty)} key={faculty.id}>
