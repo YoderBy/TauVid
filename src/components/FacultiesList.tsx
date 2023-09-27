16c291aff5cf69c637f298523eb4b91ed7047588
YoderBy <yosefbyd@gmail.com>
2023-09-27 14:38:43 +0300

    fixed the color highlight issue

--- src/components/FacultiesList.tsx
+++ src/components/FacultiesList.tsx
@@ -2,21 +2,21 @@ import { List, Text, ListItem, Button } from "@chakra-ui/react";
 import useFaculties from "../Hooks/useFaculties";
 import { Faculty } from "../Hooks/useFaculties";
 interface Props {
-    selectedFaculty : Faculty | null; 
+    selectedFaculty: Faculty | null;
     onSelect: (faculty: Faculty) => void;
-}   
-const FacultiesList = ({ onSelect, selectedFaculty}: Props) => {
+}
+const FacultiesList = ({ onSelect, selectedFaculty }: Props) => {
     const genre = useFaculties(); // generate faculty[] object
     return (
         <List overflowWrap={'break-word'} w={{ base: "100px", md: "200px" }}>
             {genre.map(gen =>
                 <ListItem
-                background={gen.id === selectedFaculty?.id ? '#f8f8ff' : ''} 
-                border="1px" paddingY={'10px'} key={gen.id}>
-                    <Button 
+                    background={gen.id === selectedFaculty?.id ? '#f8f8ff' : ''}
+                    border="1px" paddingY={'10px'} key={gen.id}>
+                    <Button
                         maxWidth="100%" whiteSpace="normal" variant='link'
                         fontWeight={gen.id === selectedFaculty?.id ? 'bold' : 'normal'}
-                        
+                        color={gen.id === selectedFaculty?.id ? 'black' : ''}
                         onClick={() => onSelect(gen)} fontSize={{ base: "10px", md: "15px" }}>
                         {gen.name}
                     </Button>
