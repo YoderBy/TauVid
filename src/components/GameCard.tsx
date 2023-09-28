d81e5b73286bd3af3123f5e56d56e923aabb0580
YoderBy <yosefbyd@gmail.com>
2023-09-28 22:21:53 +0300

    fixed image height

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ const GameCard = ({video} : Props)=>
     // this component styling can and should be imnporoved
     <Card width='100%' overflow={'hidden'}  padding = {{sm:'10px', lg:0}}borderRadius={'10px'}>
             <Link href={video.detailUrl} isExternal>
-            <Image width = "100%" maxH={{sm: 'fr1', lg: '200px'}} objectFit= 'fill' src={video.thumbnailUrl}></Image>
+            <Image width = "100%" h={{sm: 'fr1', lg: '140px'}} objectFit= 'fill' src={video.thumbnailUrl}></Image>
                 </Link>
         <CardBody>
             <Text noOfLines={2} dir='rtl' fontSize={'md'}>{video.name}</Text>
